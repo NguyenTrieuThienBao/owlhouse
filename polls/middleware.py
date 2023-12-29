f57e148e6903d52fcc49b88147942ec5e4f3387f
Kanye22 <124656776+TuanCui22@users.noreply.github.com>
2023-12-29 18:19:37 +0700

    Update middleware.py

--- polls/middleware.py
+++ polls/middleware.py
@@ -6,8 +6,14 @@ class LoginRequiredMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # Gọi hàm xử lý chính của middleware
         response = self.get_response(request)
-        if not request.user.is_authenticated and request.path == reverse('polls:cart'):
-            return redirect('http://127.0.0.1:8000/login/') 
 
+        # Kiểm tra xem người dùng có đăng nhập và đang truy cập trang 'polls:cart' không
+        if not request.user.is_authenticated and request.path == reverse('polls:cart'):
+            # Nếu không đăng nhập, chuyển hướng người dùng đến trang đăng nhập
+            return redirect('http://127.0.0.1:8000/login/')
+
+        # Trả về phản hồi
         return response
+
