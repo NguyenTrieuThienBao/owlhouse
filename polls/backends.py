c90079a3cee8a79b9a7bf43801973e35b9a7712c
Kanye22 <124656776+TuanCui22@users.noreply.github.com>
2023-12-29 18:19:06 +0700

    Update backends.py

--- polls/backends.py
+++ polls/backends.py
@@ -5,9 +5,13 @@ from polls.models import CustomUser
 class CustomUserModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
+            # Lấy người dùng từ cơ sở dữ liệu dựa trên tên đăng nhập
             user = CustomUser.objects.get(username=username)
         except CustomUser.DoesNotExist:
+            # Trả về None nếu không tìm thấy người dùng
             return None
 
+        # Kiểm tra mật khẩu sử dụng hàm check_password của Django
         if check_password(password, user.password) and self.user_can_authenticate(user):
+            # Trả về người dùng nếu mật khẩu hợp lệ và người dùng có thể xác thực
             return user
